f652c1d58bab937649b2519c9c6515224da9ef1c
Orazniyaz Chollaev <orazchollaev@gmail.com>
2026-05-16 16:08:30 +0500

    feat: persist teams and tournaments in localStorage

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
+import { createPersistedStatePlugin } from "pinia-plugin-persistedstate-2"
+
 import router from "./router"
 import App from "./App.vue"
 
 import "./assets/style/index.css"
 
-const app = createApp(App)
 const pinia = createPinia()
+const persistedStatePlugin = createPersistedStatePlugin({
+  storage: window.localStorage,
+})
+pinia.use(persistedStatePlugin)
+
+const app = createApp(App)
+
 app.use(pinia)
 app.use(router)
 app.mount("#app")
