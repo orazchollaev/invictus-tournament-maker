1acffef205305894a4d0326fd921e23a5723f394
Orazniyaz Chollaev <orazchollaev@gmail.com>
2026-07-15 18:58:44 +0500

    fix(build): load fontsource font via CSS import to avoid TS side-effect import error

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import App from "./App.vue"
 import i18n, { loadLocale } from "./i18n"
 import type { Locale } from "./i18n"
 
-import "@fontsource-variable/ibm-plex-sans"
 import "./assets/style/index.css"
 
 const pinia = createPinia()
