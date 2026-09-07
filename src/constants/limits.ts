3ff71d24e5f3775073272c4bf84c92cd15b04307
Oraz Chollayev <orazchollaev@gmail.com>
2026-09-07 09:48:10 +0500

    feat(tournament): raise max team cap to 1000

--- src/constants/limits.ts
+++ src/constants/limits.ts
@@ -1,5 +1,5 @@
 /** Upper bound on participants in a single tournament. */
-export const MAX_TEAMS = 256
+export const MAX_TEAMS = 1000
 
 /** Upper bound on goals enterable for one side of a match. */
 export const MAX_GOALS = 20
